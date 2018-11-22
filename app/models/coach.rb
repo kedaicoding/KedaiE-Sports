427f949a5ba333bae899a947f3fc72570a91b452
Maulana Hilmi <maulanahilmi1331@gmail.com>
2018-11-22 20:05:04 +0700

    fix model coach

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,8 +1,8 @@
 class Coach < ApplicationRecord
-    belongs_to :team
-    has_many :scheduleable, :as => :scheduleable
-    has_many :track_record_coaches
-    has_many :gallery_coaches
+    # belongs_to :team
+    # has_many :scheduleable, :as => :scheduleable
+    # has_many :track_record_coaches
+    # has_many :gallery_coaches
   # Include default devise modules. Others available are:
     #:omniauthable
   devise :database_authenticatable, :registerable,
